77c4fa7cefce6439893d1798ecb4059df99bf69a
Marcelo <mpezzolano@gmail.com>
2020-05-02 14:04:49 -0300

    Change Pom Conection & SQL Script

--- scripts/script-tabla-leads.sql
+++ scripts/script-tabla-leads.sql
@@ -1,10 +1,17 @@
 --
 -- Table structure for table `Albums`
 --
-CREATE TABLE `Leads` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(200) NOT NULL,
-  `mail` varchar(200) NOT NULL,
-  `ip` varchar(150) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+-- CREATE TABLE `Leads` (
+--  `id` int(11) NOT NULL AUTO_INCREMENT,
+--  `nombre` varchar(200) NOT NULL,
+--  `mail` varchar(200) NOT NULL,
+--  `ip` varchar(150) NOT NULL,
+--  PRIMARY KEY (`id`)-
+-- ) ENGINE=MyISAM;
+
+CREATE TABLE leads(
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    mail  VARCHAR(40) NOT NULL,
+    ip VARCHAR(40),
+    PRIMARY KEY ( id ) );
